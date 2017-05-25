d40108d1dd3a2c22384c39ce22470309081cdf4f
Kevin Arnold Arias Figueroa <KAAF030191@gmail.com>
2017-05-25 18:32:28 -0500

    Reemplazo de proyectos web por librería de clases en toda la arquitectura necesaria.

--- 4.1.DataAccessRepository/Properties/AssemblyInfo.cs
+++ 4.1.DataAccessRepository/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("_4._1.DataAccessRepository")]
+[assembly: AssemblyTitle("4.1.DataAccessRepository")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("_4._1.DataAccessRepository")]
+[assembly: AssemblyProduct("4.1.DataAccessRepository")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("040665cc-8c6e-48ba-a5cc-ace82c1b2364")]
+[assembly: Guid("96b8b280-33dd-4f80-9e4e-90b754c0324d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
