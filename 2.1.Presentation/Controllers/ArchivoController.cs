6e9780c61f089d60c93a4118992c87a7b6ec65a3
jcebcarlos <jcebcarlos@gmail.com>
2017-05-26 11:55:49 -0500

    Modificando plantilla de layout segun requerimiento de la sistema

---
+++ 2.1.Presentation/Controllers/ArchivoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _2._1.Presentation.Controllers
+{
+    public class ArchivoController : Controller
+    {
+        // GET: Archivo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
